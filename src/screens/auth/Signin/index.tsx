5135c4b1c2e61ab4e18145f1e22f1d97449e4cbe
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-07 19:11:11 +0530

    Sign in with existing to user

--- src/screens/auth/Signin/index.tsx
+++ src/screens/auth/Signin/index.tsx
@@ -1,18 +1,68 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
 import styles from './styles';
 import Button from '../../../components/Button';
 import Title from '../../../components/Title';
 import Input from '../../../components/Input';
 import {NavigationProp} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
-const Signin = ({navigation}: {navigation: NavigationProp<any>}) => {
+interface SigninValues {
+  email: string;
+  password: string;
+}
+
+interface SigninProps {
+  navigation: NavigationProp<any>;
+}
+
+const Signin: React.FC<SigninProps> = ({navigation}) => {
+  const [values, setValues] = React.useState<SigninValues>({
+    email: '',
+    password: '',
+  });
+
+  const onChange = (value: string, key: keyof SigninValues) => {
+    setValues(vals => ({
+      ...vals,
+      [key]: value,
+    }));
+  };
+
+  const onSubmit = () => {
+    auth()
+      .signInWithEmailAndPassword(values.email, values.password)
+      .then(() => {
+        console.log('User signed in!');
+      })
+      .catch(error => {
+        console.error('error:>>',error);
+        if (error.code === 'auth/user-not-found') {
+          console.log('No user found with this email!');
+        } else if (error.code === 'auth/wrong-password') {
+          console.log('Incorrect password!');
+        } else if (error.code === 'auth/invalid-email') {
+          console.log('Invalid email format!');
+        }else {
+          Alert.alert(error.message);
+        }
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Title title="Welcome back" />
-      <Input placeholder="Email" keyboardType="email-address" />
-      <Input placeholder="Password" secureTextEntry />
-      <Button title="login" />
+      <Input
+        placeholder="Email"
+        keyboardType="email-address"
+        onChangeText={value => onChange(value, 'email')}
+      />
+      <Input
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={value => onChange(value, 'password')}
+      />
+      <Button title="Login" onPress={onSubmit} />
       <Text style={[styles.footerText]}>
         Not Registered?
         <Text
